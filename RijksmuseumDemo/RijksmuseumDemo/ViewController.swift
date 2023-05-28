5ad852726babd7efe8a78e54eae3455f09d683cc
Roman Churkin <roman.churkin@teampicnic.com>
2023-05-28 21:03:09 +0200

    Initialise project

---
+++ RijksmuseumDemo/RijksmuseumDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RijksmuseumDemo
+//
+//  Created by Roman Churkin on 28/05/2023.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
